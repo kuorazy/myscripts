68ab77bfc1be7c0f1028f5a3a2c98a2b6b2f1cdf
gzy <737001160@qq.com>
2024-03-25 23:32:02 +0800

    test network

--- src/service/network_util.py
+++ src/service/network_util.py
@@ -6,7 +6,7 @@ class PythonPing:
     def execute():
         ping("www.baidu.com", verbose=True)
 
-        ping("smtp.qq.com", verbose=True)
+        # ping("smtp.qq.com", verbose=True)
 
         ping("www.google.com", verbose=True)
 
